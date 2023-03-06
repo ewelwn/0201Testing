93daff8ba50c5a4033dbae578a41c138de6b3579
ewelwn <122063004+ewelwn@users.noreply.github.com>
2023-03-05 20:36:26 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-var dice = {
-  sides: 6,
-  roll: function () {
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
-  }
-}
+var rollButton = document.getElementById("btn");
+var text = document.getElementById("dice");
+
+rollButton.addEventListener("click",function(){
+  var result = Math.floor(Math.random() * 6) + 1;
+    if(result == 1){
+    text.style.backgroundImage = "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnfIHKtmZgYnwFYn6yQPjq-4fw85pwbpY5CyG9h--P6A&s)";
+    }
+    else if (result == 2){
+    text.style.backgroundImage = "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvTLuMJFTUBiEN743p9rK_2rE8d-xKy8twSCc7lHsXiA&s)";
+    }
+    else if (result == 3){
+    text.style.backgroundImage = "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6JfWSgJXwzsTepkgbUr3bdpBdU2zmnbT_NR_ReC-GaQ&s)";
+    }
+    else if (result == 4){
+    text.style.backgroundImage = "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxok6EwasSr_XhmH4Emf7BFh-cMYzWcp4v_RsWwvcN6Q&s)";
+    }
+    else if (result == 5){
+    text.style.backgroundImage = "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1yR8TFH0zpQxZaLnAqyqOJI8YFbdq2dE1XVhAtSCHrw&s)";
+    } 
+    else if (result == 6){
+    text.style.backgroundImage = "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy_WQ23MUzdX935ugZhRWMhmmHGw5mrJUmVs6QDJUbLA&s)";
+    }
+});
 
 
-
-//Prints dice roll to the page
-
-function printNumber(number) {
-  var placeholder = document.getElementById('placeholder');
-  placeholder.innerHTML = number;
-}
-
-var button = document.getElementById('button');
-
-button.onclick = function() {
-  var result = dice.roll();
-  printNumber(result);
-};
-
-//document.getElementById("myDIV").style.display = "none"
